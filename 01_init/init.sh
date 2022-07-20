345fe01533831164885bf5021b19579333d4aaa4
Alexdelia <alexandre.delille.57@gmail.com>
2022-07-20 13:40:43 +0200

    tsc --init

--- 01_init/init.sh
+++ 01_init/init.sh
@@ -4,6 +4,6 @@ sudo npm init
 
 sudo npm install --save-dev lite-server
 
-echo "create a run rule in the script section of package.json"
-
 tsc --init
+
+echo "  create a run rule in the script section of package.json"
