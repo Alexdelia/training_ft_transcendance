bf270cd434c8a14d91c63a53bc1551f45a9366ad
adelille <adelille@student.42.fr>
2022-07-27 13:04:30 +0200

    saving

--- 04_react/src/components/user.component.tsx
+++ 04_react/src/components/user.component.tsx
@@ -1,17 +1,28 @@
-//import { useState } from "react";
+import { useState } from "react";
+
+import { Backdrop } from "./Backdrop";
 
 interface i_user {
 	name: string;
 }
 
 function User(props: i_user) {
+	const [showChat, setShowChat] = useState(false);
+	const [showProfile, setShowProfile] = useState(false);
+
+	function resetAllStateHandle() {
+		setShowChat(
+	}
+
 	return (
-		<div className='card'>
+		<div className="card">
 			<h2>{props.name}</h2>
-			<div className='action'>
-				<button className='btn'>💬</button>
-				<button className='btn'>👤</button>
+			<div className="action">
+				<button className="btn" onClick={() => { setShowChat(true) }}>💬</button>
+				<button className="btn" onClick={() => { setShowProfile(true) }}>👤</button>
 			</div>
+			{(showChat || showProfile) && <Backdrop />}
+			{showChat && <ChatModal srcName='adelille' dstName={props.name} onClose={() => { setShowChat(false) }} />
 		</div>
 	);
 }
